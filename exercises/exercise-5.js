4cfc70a7f84edf36002aaa172f58bb05791111bb
p18750 <david.lichtsteiner@css.ch>
2023-11-08 16:22:13 +0100

    Migrate to ts

--- exercises/exercise-5.js
+++
@@ -1,7 +0,0 @@
-// Exercise 5: Write a function that doubles each Number of an array.
-
-function dobule(numbers) {
-    return numbers.map(number => number * 2);
-}
-
-module.exports = dobule;
--- exercises/exercise-9.js
+++
@@ -1,9 +0,0 @@
-// Exercise 9: Calculate the total value of the Shopping Cart.
-
-function getShoppingCartTotal(shoppingCart) {
-    return shoppingCart
-        .map(item => item.quantity * item.unitPrice)
-        .reduce((totalPrice, itemPrice) => totalPrice + itemPrice, 0);
-}
-
-module.exports = getShoppingCartTotal;
